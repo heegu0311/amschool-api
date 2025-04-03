ca6780c7f82ec0558d9e46d48a031ef614ebe4f4
joseph <heegu0311@gmail.com>
2025-04-04 00:13:50 +0900

    feat: Add cancer-user entity

--- src/cancer-user/cancer-user.module.ts
+++ src/cancer-user/cancer-user.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CancerUserService } from './cancer-user.service';
-import { CancerUserController } from './cancer-user.controller';
 import { CancerUser } from './entities/cancer-user.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CancerUser])],
-  controllers: [CancerUserController],
   providers: [CancerUserService],
   exports: [CancerUserService],
 })
