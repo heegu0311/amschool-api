eebf584c83310044eaf67247fad38036a0207971
joseph <heegu0311@gmail.com>
2025-06-14 11:59:50 +0900

    config: typeorm migration 초기화

--- database/migrations/1746771299440-Migration.ts
+++
@@ -1,9 +0,0 @@
-// 개발 오늘의나, 건강 Q&A 까지 개발된 마이그레이션 버전
-
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class Migration1746771299440 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
-
-  public async down(queryRunner: QueryRunner): Promise<void> {}
-}
