4f02cbc5ddc5650f57ce0c32e16ec6cb1fb39713
joseph <heegu0311@gmail.com>
2025-06-15 00:02:35 +0900

    fix: article image upload

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -44,7 +44,7 @@ export class ArticleService {
     const now = new Date();
     const year = now.getFullYear();
     const month = String(now.getMonth() + 1).padStart(2, '0');
-    return `${year}${month}/`;
+    return `${year}${month}`;
   }
 
   private generateFileName(originalName: string): string {
